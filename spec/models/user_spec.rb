54c8556815ea6a9e75befa60bdd3ad0c9f84a2d5
nakanokosuke <jaj4fv9ec@icloud.com>
2019-06-11 23:56:32 +0900

    no message

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    expect(FactoryBot.build(:user)).to be_valid
-  end
-
-  it "is valid with a first name, last name and email, and password" do
-    user = User.new(
-      first_name: "Aaron",
-      last_name:  "Sumner",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    expect(user).to be_valid
-  end
-
-  it { is_expected.to validate_presence_of :first_name }
-  it { is_expected.to validate_presence_of :last_name }
-  it { is_expected.to validate_presence_of :email }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-
-  it "returns a user's full name as a string" do
-    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
-    expect(user.name).to eq "John Doe"
-  end
-
-  it "sends a welcome email on account creation" do
-    allow(UserMailer).to \
-      receive_message_chain(:welcome_email, :deliver_later)
-    user = FactoryBot.create(:user)
-    expect(UserMailer).to have_received(:welcome_email).with(user)
-  end
-
-  it "performs geocoding", vcr: true do
-    user = FactoryBot.create(:user, last_sign_in_ip: "161.185.207.20")
-    expect {
-      user.geocode
-    }.to change(user, :location).
-      from(nil).
-      to("Brooklyn, New York, US")
-  end
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with a first name, last name, email, and password" # 名がなければ無効な状態であること
+  it "is invalid without a first name"
+  # 姓がなければ無効な状態であること
+  it "is invalid without a last name"
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address"
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address"
+  # ユーザーのフルネームを文字列として返すこと
+  it "returns a user's full name as a string"
 end
