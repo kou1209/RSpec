9625456591233b627e9f867d0c86e70c979d694e
nakanokosuke <jaj4fv9ec@icloud.com>
2019-06-14 02:29:53 +0900

    no message

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,28 @@
 FactoryBot.define do
   factory :project do
-    
+    sequence(:name) { |n| "Test Project #{n}" }
+    description "Sample project for testing purposes"
+    due_on 1.week.from_now
+    association :owner
+
+    # メモ付きのプロジェクト
+    trait :with_notes do
+      after(:create) { |project| create_list(:note, 5, project: project) }
+    end
+
+    # 昨日が締め切りのプロジェクト
+    trait :due_yesterday do
+      due_on 1.day.ago
+    end
+
+    # 今日が締め切りのプロジェクト
+    trait :due_today do
+      due_on Date.current.in_time_zone
+    end
+
+    # 明日が締め切りのプロジェクト
+    trait :due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
